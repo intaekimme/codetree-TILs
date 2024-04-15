591bf1f340a0f9dba158caea25e48463760ecb4e
InTae Kim <kit938639@gmail.com>
2024-04-15 22:12:15 +0900

    Title: 구간 중 최대 합, Time: 113ms, Memory: 8MB, Status: Passed - CodeTree

--- 240415/구간 중 최대 합/max-sum-of-subarray.java
+++ 240415/구간 중 최대 합/max-sum-of-subarray.java
@@ -20,7 +20,7 @@ public class Main {
         }
 
         int max = 0;
-        for (int i = 0; i < n - k; i++) {
+        for (int i = 0; i <= n - k; i++) {
             int sum = 0;
             for (int j = i; j < i + k; j++) {
                 sum += arr[j];
